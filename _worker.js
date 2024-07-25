3cbf667ef6e2b4e8e3569ce8e3c95bff8e208f67
hcxhb <hcxhb@hotmail.com>
2024-07-25 20:32:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '0be9e40b-2e02-43e1-a5fc-7b6cce8e03cb';
+let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
